f5c52d86b353d7cbc1a5965d17d4c4bff38ce54e
NingLiu587 <liuning587@qq.com>
2013-03-10 22:05:27 +0800

    提交assert_param

--- stm32f2lib/inc/stm32f2xx.h
+++ stm32f2lib/inc/stm32f2xx.h
@@ -109,6 +109,23 @@
   #define HSI_VALUE    ((uint32_t)16000000) /*!< Value of the Internal oscillator in Hz*/
 #endif /* HSI_VALUE */
 
+
+ /* Exported macro ------------------------------------------------------------*/
+ #ifdef  USE_FULL_ASSERT
+
+  /**
+    * @brief  The assert_param macro is used for function's parameters check.
+    * @param  expr: If expr is false, it calls assert_failed function which reports
+    *         the name of the source file and the source line number of the call
+    *         that failed. If expr is true, it returns no value.
+    * @retval None
+    */
+    #define assert_param(expr) ((expr) ? (void)0 : assert_failed((uint8_t *)__FILE__, __LINE__))
+ /* Exported functions ------------------------------------------------------- */
+    void assert_failed(uint8_t* file, uint32_t line);
+ #else
+    #define assert_param(expr) ((void)(expr))
+ #endif /* USE_FULL_ASSERT */
 /**
  * @brief STM32F2XX Standard Peripherals Library version number V1.1.3
    */
