e772c8313ab875786d32a8925fb3699dbfdcbb57
NingLiu587 <liuning587@qq.com>
2013-03-29 13:55:28 +0800

    支持lm3s811 board

---
+++ mxapp/bsp/board/ti811/bsplib.c
@@ -0,0 +1,124 @@
+/**
+ ******************************************************************************
+ * @file      bsplib.c
+ * @brief     C Source file of bsplib.c.
+ * @details   This file including all API functions's 
+ *            implement of bsplib.c.	
+ *
+ * @copyright
+ ******************************************************************************
+ */
+ 
+/*-----------------------------------------------------------------------------
+ Section: Includes
+ ----------------------------------------------------------------------------*/
+#include "../../bspcfg.h"
+#include <types.h>
+#include <intLib.h>
+#include <lm3s811.h>
+
+#if (BOARD_BUILD_VER == BOARD_TI811)
+
+/*-----------------------------------------------------------------------------
+ Section: Type Definitions
+ ----------------------------------------------------------------------------*/
+/* NONE */
+
+/*-----------------------------------------------------------------------------
+ Section: Constant Definitions
+ ----------------------------------------------------------------------------*/
+/* NONE */ 
+
+/*-----------------------------------------------------------------------------
+ Section: Global Variables
+ ----------------------------------------------------------------------------*/
+/* NONE */
+
+/*-----------------------------------------------------------------------------
+ Section: Local Variables
+ ----------------------------------------------------------------------------*/
+/* NONE */
+
+/*-----------------------------------------------------------------------------
+ Section: Local Function Prototypes
+ ----------------------------------------------------------------------------*/
+/* NONE */
+
+/*-----------------------------------------------------------------------------
+ Section: Function Definitions
+ ----------------------------------------------------------------------------*/
+void
+mcuClkSetup(void)
+{
+    return;
+#if PLL_EN == 0
+    /*  Not use PLL  不使用PLL      */
+    SysCtlClockSet(CCLK_DIV
+            | SYSCTL_USE_OSC
+            | SYSCTL_OSC_MAIN
+            | EXT_CLK);
+ #else
+    /*  Use PLL  使用PLL            */
+    SysCtlClockSet(CCLK_DIV
+            | SYSCTL_USE_PLL
+            | SYSCTL_OSC_MAIN
+            | EXT_CLK);
+#endif
+}
+
+void
+bspHwInit(void)
+{
+    intLock();
+
+    intUnlock();
+}
+
+int32_t bsp_getchar(void)
+{
+    int32_t c;
+
+    c = (int32_t)UARTCharGetNonBlocking(UART0_BASE);
+    if (c < 0)
+        c = 0;
+    return c;
+}
+
+void bsp_putchar(char_t ch)
+{
+    UARTCharPut(UART0_BASE, ch);
+}
+
+void bsp_reboot(void)
+{
+    SysCtlReset();
+}
+
+void
+bsp_timer_start(void)
+{
+
+}
+
+uint32_t
+bsp_timer_get(void)
+{
+    return 0;
+}
+
+/* 5. 获取MCU主频 */
+uint32_t
+bsp_get_mcu_clk(void)
+{
+    return CPU_CLOCK_HZ;
+}
+
+/* 6. 获取MCU中断数量 */
+uint32_t
+bsp_get_max_int_count(void)
+{
+    return MAX_INT_COUNT;
+}
+
+#endif
+/*--------------------------------bsplib.c-----------------------------------*/
